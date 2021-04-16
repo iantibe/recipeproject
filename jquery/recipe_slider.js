72f69eebce84e99a26ebaf4f27a488e4e5bb2fe1
Ian Tibe <imtibe@dmacc.edu>
2021-04-16 01:03:55 -0500

    started recipe display page

---
+++ jquery/recipe_slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#display_header_1").click(function(){
+        $("#display_info1").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#display_header_2").click(function(){
+        $("#display_info2").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#display_header_3").click(function(){
+        $("#display_info3").slideToggle("slow");
+    });
+});
